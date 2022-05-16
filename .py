5334bc7f538d06269a582712fda7d86ec7fcd77d
Aashritha373 <105585311+Aashritha373@users.noreply.github.com>
2022-05-16 21:34:21 +0530

    Updated .py

--- .py
+++ .py
@@ -7,3 +7,17 @@ EXTENSION OF FILE
 f=input("Input the Filename:")
 f_e = f.split(".")
 print("The extension of the file is : "+repr(f_e[-1]))
+
+FIBONACCI SERIES
+n=int(input("Number of elements in fibonacci series"))
+a=0
+b=1 
+sum=0
+print(a)
+print(b)
+for i in range(2,n):
+    sum = a+b 
+    a=b
+    b=sum
+    print(sum)    
+    
