a58aa55063a117a5ca6f609aa969cbd2eba17817
Aashritha373 <105585311+Aashritha373@users.noreply.github.com>
2022-05-16 20:57:16 +0530

    Created .py

---
+++ .py
@@ -0,0 +1,9 @@
+AREA OF CIRCLE
+r=float(input("Enter the radius of the circle : "))
+area = 3.14*r**2
+print("The area of the circle with radius",r,"is:",area)
+
+EXTENSION OF FILE
+f=input("Input the Filename:")
+f_e = f.split(".")
+print("The extension of the file is : "+repr(f_e[-1]))
